b574dadfab07c547188c820d8931f8ccb76749f4
Richard Sykes <rich.sykes@hotmail.co.uk>
2017-08-06 20:27:41 +0100

    Included Unix path support and option to run without sending orders.

--- gdax_automated_trading.py
+++ gdax_automated_trading.py
@@ -1,56 +1,52 @@
 
 # coding: utf-8
 
-# In[ ]:
 
 import gdax
 import time
 from datetime import datetime
 import pandas as pd
-
+import sys
 public_client = gdax.PublicClient()
 
-
-# In[ ]:
 
 # Get the order book at the default level.
 #public_client.get_product_order_book('BTC-GBP')
 # Get the order book at a specific level.
 #public_client.get_product_order_book('BTC-GBP', level=1)
 
-
-# In[ ]:
-
 # Get the product ticker for a specific product.
 public_client.get_product_ticker(product_id='BTC-GBP')
-
-
-# In[ ]:
 
 # Get the product trades for a specific product.
 #public_client.get_product_trades(product_id='BTC-GBP')
 
-
-# In[ ]:
 
 public_client.get_product_historic_rates('BTC-GBP')
 # To include other parameters, see function docstring:
 public_client.get_product_historic_rates('BTC-GBP', granularity=3000)
 
 
-# In[ ]:
-
 # login text file... see readme
-login = pd.read_csv(r'C:\dev\gdax\login.txt')
+
+if sys.platform == 'linux':
+    print("linux")
+    login = pd.read_csv('/home/richard/dev/login.txt')
+
+else:
+    print("windows")
+   # login = pd.read_csv(r'C:\dev\gdax\login.txt')
+
+
+
 auth_client = gdax.AuthenticatedClient(login["BLANK LINE"][1], login["BLANK LINE"][2], login["BLANK LINE"][0])
 
-
-# In[ ]:
 
 def placeBuyOrSellOrder():
 
     running = input
-    
+    tradingActive = False
+
     # define fiat account
     gbp_acc = auth_client.get_account(login["BLANK LINE"][3])
     
@@ -84,30 +80,32 @@ def placeBuyOrSellOrder():
     if float(gbp_acc["available"]) > latest_price*btc_trade_vol*btc_trade_margin:
         
         print('BUY order £' + target_buy + " : Mid Market £" +str(mid_market) + " : "+ str(datetime.now()))
-        
-        auth_client.buy(price=target_buy,
-               size=btc_trade_vol, #BTC
-               product_id='BTC-GBP')
+
+        if tradingActive == True:
+            auth_client.buy(price=target_buy,
+                size=btc_trade_vol, #BTC
+                product_id='BTC-GBP')
+        else:
+            print(str(target_buy))
 
     # SELL if BTC availble
     elif float(btc_acc["available"]) > btc_trade_vol:
         
         print('SELL order ' + target_sell + " : Bid Market £" +str(price_bid) + " : "+ str(datetime.now()))
 
-        auth_client.sell(price=target_sell,
+        if tradingAcivte == True:
+            auth_client.sell(price=target_sell,
                          size=str(btc_trade_vol), #BTC
                          product_id='BTC-GBP')
-            
+        else:
+            print(str(target_sell))
+
     else:
         
         time.sleep(30)
                 
     
 
-        
-
-
-# In[ ]:
 
 # trigger object
 while 1<2:
