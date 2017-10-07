5dbe2bf177a3291aa47e76481a1617a1c712092e
Rich <rich.sykes@hotmail.co.uk>
2017-10-07 23:56:57 +0100

    Initial commit of object based structure

--- gdax_automated_trading.py
+++ gdax_automated_trading.py
@@ -2,10 +2,15 @@ import gdax
 # import time
 # from datetime import datetime
 import pandas as pd
+
+
 # import sys
 
 
 class CryptoTrade:
+    # datetime = __import__("datetime")
+    # threading = __import__("threading")
+
     def __init__(self, name):
 
         self.name = name
@@ -63,19 +68,18 @@ class CryptoTrade:
     def get_targets(self, product):
 
         bsg = 0.25  # buy/sell gain
-        # max_buy_price = self.get_bid(product) * (1 - (bsg / 100))
-        # min_sell_price = self.get_ask(product) * (1 + (bsg / 100))
         g_s_b = self.get_ask_bid(product)
 
         max_buy_price = g_s_b["bid"] * (1 - (bsg / 100))
         min_sell_price = g_s_b["ask"] * (1 + (bsg / 100))
+
         target_ask = round(max(max_buy_price, min_sell_price), 2)
         target_bid = round(min(max_buy_price, min_sell_price), 2)
         return {"target_ask": target_ask, "target_bid": target_bid}
 
     def trade_market_maker(self, product):
 
-        trade_size = 0.05
+        # put bids in below latest ask -  and asks in above latest bid
 
         # a = float(self.get_ask(product))
         # b = float(self.get_bid(product))
@@ -88,27 +92,47 @@ class CryptoTrade:
         t_a = float(targets["target_ask"])
         t_b = float(targets["target_bid"])
 
-        fiat_acc = float(self.get_acc_balance(product.split(sep="-")[1]))  # EUR
-        cryp_acc = float(self.get_acc_balance(product.split(sep="-")[0]))  # ETH
+        fiat_acc = float(self.get_acc_balance(product.split(sep="-")[1]))  # e.g. EUR
+        cryp_acc = float(self.get_acc_balance(product.split(sep="-")[0]))  # e.g. ETH
+
+        # 25 of fiat currency / target bid of one unit
+        # GDAX order constraint
+        trade_size = max(round((10 / t_b), 6), 0.01)
+
+        # messages
+        print("Ask: " + str(a) + " and Target Ask " + str(t_a))
+        print("Bid: " + str(b) + " and Target Bid " + str(t_b))
 
         if fiat_acc > t_a * trade_size:
             print("Buy Crypto Possible")
-            print("Ask: " + str(a) + " and Target Ask " + str(t_a))
-            print("Bid: " + str(b) + " and Target Bid " + str(t_b))
-            # self.auth_client.buy(price=t_b, size=trade_size, product_id=product)
+            print("Buying at " + str(t_b))
+            # self.auth_client.buy(price=str(t_b), size=str(trade_size), product_id=product)
+            self.auth_client.buy(price=str(t_b), size=str(trade_size), product_id=product,
+                                 time_in_force="GTT", cancel_after="min")
+
         else:
-            print("Insufficient Balance")
-            # self.auth_client.sell(price=t_a, size=trade_size, product_id=product)
+            print("Insufficient " + product.split(sep="-")[1] + " fund to buy.")
 
         if cryp_acc > trade_size:
             print("Sell Crypto Possible")
-            print("Ask: " + str(a) + " and Target Ask " + str(t_a))
-            print("Bid: " + str(b) + " and Target Bid " + str(t_b))
+            print("Selling at " + str(t_a))
+            # self.auth_client.sell(price=str(t_a), size=str(trade_size), product_id=product)
+            self.auth_client.sell(price=str(t_a), size=str(trade_size), product_id=product,
+                                  time_in_force="GTT", cancel_after="min")
+
         else:
-            print("Insufficient Balance")
-            # self.auth_client.sell(price=t_a, size=trade_size, product_id=product)
+            print("Insufficient " + product.split(sep="-")[0] + " to sell.")
+
+    def run_trade(self, product):
+        import time
+
+        while True:
+            time.sleep(25)
+            self.trade_market_maker(product)
 
 
-# trade = CryptoTrade("trade")
-# trade.trade_market_maker("ETH-EUR")
+trade = CryptoTrade("trade")
 
+trade.run_trade("BTC-EUR")
+
+# EOF
